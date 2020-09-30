3764427d3c29495e98f69cb70428ae9d6a701983
Kent-EGUCHI <66657775+Kent-EGUCHI@users.noreply.github.com>
2020-09-30 12:25:29 +0900

    Update avg_std_jitter.py

--- avg_std_jitter.py
+++ avg_std_jitter.py
@@ -25,26 +25,28 @@ sns.set(
 )
 
 jitter = []
-interval = 0.0005
+interval = 0.0001
 sample = 100000
+width = 0.0001
 
 #with open('/home/eguchi/ownCloud/Documents/raspberrypi/time_readadc_spidev',) as rf:
 #    t = rf.readlines()
 #t = np.loadtxt('/home/eguchi/ownCloud/Documents/raspberrypi/time_vs_bit/spidev_output_interval_'+str(interval)+'.txt')
-t = np.loadtxt('E:/owncloud/Documents/raspberrypi/adc_photor/jitter_output_interval='+str(interval)+'s_sample='+str(sample))
+t = np.loadtxt('E:/owncloud/Documents/raspberrypi/adc_photor/spidev_output_interval='+str(interval)+'s_sample='+str(sample))
 #with open('/home/eguchi/ownCloud/Documents/raspberrypi/time_vs_bit/spidev_output_interval_0.0005.txt', mode = 'w') as wf:
 #    print("avg",statistics.mean(t),file=wf)
 #    print("stdev",statistics.stdev(t),file=wf)
 #    print("var",math.sqrt(numpy.var(t,ddof=1)),file=wf)
 
 #print(np.shape(t)[0])
-for i in range(np.shape(t)[0]):
-    jitter.append(t[i][0]-t[i-1][0])
+for i in range(np.shape(t)[0]-1):
+    jitter.append(t[i+1][0]-t[i][0])
 
-jitter = jitter - interval
+for i in range(np.shape(jitter)[0]):
+    jitter[i] = jitter[i] - interval
 
 #with open('/home/eguchi/ownCloud/Documents/raspberrypi/time_vs_bit/std_avg_spidev_output_interval_'+str(interval), mode = 'w') as wf:
-with open('E:/owncloud/Documents/raspberrypi/adc_photor/jitter_std_avg_interval_'+str(interval), mode = 'w') as wf:
+with open('E:/owncloud/Documents/raspberrypi/adc_photor_jitter/jitter_std_avg_interval_'+str(interval), mode = 'w') as wf:
     print("interval",'{:e}'.format(interval),file=wf)
     print("jitter_avg",statistics.mean(jitter),file=wf)
     print("stdev_sample",statistics.stdev(jitter),file=wf)
@@ -57,26 +59,26 @@ fig, ax = plt.subplots(figsize = (9,6)) #アスペクト比
 ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
 ax.set_xlabel("jitter (s)")
 
-plt.text(2.0,3.0,'(a)')
-
 ax.ticklabel_format(  # 指数表記
     style="sci",
     scilimits=(0, 0),
     axis="x"
 )
-#ma = max(jitter)
+ma = max(jitter)
+mi = min(jitter)
 #print(ma)
 
-ax.set_ylim(0,6000)
+#ax.set_ylim(0,1000)
 
 ax = plt.hist(
     jitter,
     bins=100,
-    range=(0,0.00001),
-    #range=(0,ma),
+    range=(-1*width,width),
+    #range=(mi,ma),
 )
 
 
 #plt.savefig('E:/owncloud/Documents/raspberrypi/adc_photor/jitter_hist_interval='+str(interval)+'s_sample='+str(sample)+'.png')
-plt.savefig('E:/owncloud/Documents/raspberrypi/adc_photor/jitter_hist_interval='+str(interval)+'s_sample='+str(sample)+'.png')
+plt.savefig('E:/owncloud/Documents/raspberrypi/adc_photor_jitter/jitter_hist_interval='+str(interval)+'s_sample='+str(sample)+'width='+str(width)+'.png')
+#plt.savefig('E:/owncloud/Documents/raspberrypi/adc_photor_jitter/jitter_wide_hist_interval='+str(interval)+'s_sample='+str(sample)+'.png')
 #plt.hist(sample_rate)
